7972318222b8fa0df8930edd7ae3e2e648ef96d5
Karen Zhao <kzhao710@gmail.com>
2025-08-20 12:34:23 -0700

    change git link

--- app/Hero/ViewGitHubButton.tsx
+++ app/Hero/ViewGitHubButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const ViewGitHubButton = () => {
     return (
-        <Link className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-zinc-200 font-medium text-emerald-900" href="https://github.com/karenz710">
+        <Link className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-zinc-200 font-medium text-emerald-900" href="https://github.com/karzhao">
             <IoLogoGithub size={24} />
         </Link>
     );
